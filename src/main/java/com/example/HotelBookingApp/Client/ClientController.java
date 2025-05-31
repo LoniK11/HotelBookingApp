2b4349947a670e30130578ffebdbdfc0e08c5ad7
Valon Kulici <valonkulici11@gmail.com>
2025-05-31 18:07:32 +0200

    Implemented POST method for adding new Clients

--- src/main/java/com/example/HotelBookingApp/Client/ClientController.java
+++ src/main/java/com/example/HotelBookingApp/Client/ClientController.java
@@ -1,9 +1,23 @@
 package com.example.HotelBookingApp.Client;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
+
+    private ClientService clientService;
+
+    public ClientController(ClientService clientService){
+        this.clientService = clientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addNewUser(@RequestBody() ClientEntity client){
+        return this.clientService.addNewClient(client);
+    }
 }
--- src/main/java/com/example/HotelBookingApp/Client/ClientService.java
+++ src/main/java/com/example/HotelBookingApp/Client/ClientService.java
@@ -1,7 +1,20 @@
 package com.example.HotelBookingApp.Client;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
+    }
+
+    public ResponseEntity<String> addNewClient(ClientEntity client){
+         this.clientRepository.save(client);
+         return ResponseEntity.status(HttpStatus.CREATED).body("Client added successfully!");
+    }
 }
