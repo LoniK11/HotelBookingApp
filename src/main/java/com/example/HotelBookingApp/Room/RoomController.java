80be546d01f57df4c60f6a8b46d0a0c8663968cb
Valon Kulici <valonkulici11@gmail.com>
2025-06-01 10:26:06 +0200

    Implemented POST method to add new Rooms

--- src/main/java/com/example/HotelBookingApp/Room/RoomController.java
+++ src/main/java/com/example/HotelBookingApp/Room/RoomController.java
@@ -1,9 +1,29 @@
 package com.example.HotelBookingApp.Room;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/room")
 public class RoomController {
+
+    private RoomService roomService;
+
+    public RoomController(RoomService roomService){
+        this.roomService = roomService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addNewRoom(@RequestBody() RoomEntity room){
+        try{
+            RoomEntity createdRoom = this.roomService.addNewRoom(room);
+            return ResponseEntity.ok("Room added successfully");
+        }catch(IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/example/HotelBookingApp/Room/RoomService.java
+++ src/main/java/com/example/HotelBookingApp/Room/RoomService.java
@@ -1,7 +1,24 @@
 package com.example.HotelBookingApp.Room;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoomService {
+
+    private RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository){
+        this.roomRepository = roomRepository;
+    }
+
+    public RoomEntity addNewRoom(RoomEntity room){
+        if(room == null){
+            throw new IllegalArgumentException("Room cannot be null!");
+        }
+        return this.roomRepository.save(room);
+    }
+
+
 }
