2bd482810ec71816c4d418b7ebf2906db65988f3
Valon Kulici <valonkulici11@gmail.com>
2025-06-03 09:37:58 +0200

    Implemented GET method to get all Bookings

--- src/main/java/com/example/HotelBookingApp/Booking/BookingController.java
+++ src/main/java/com/example/HotelBookingApp/Booking/BookingController.java
@@ -1,9 +1,23 @@
 package com.example.HotelBookingApp.Booking;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/booking")
 public class BookingController {
+
+    private BookingService bookingService;
+
+    public BookingController(BookingService bookingService){
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping
+    public List<BookingEntity> getAllBookings(){
+        return this.bookingService.getAllBookings();
+    }
 }
--- src/main/java/com/example/HotelBookingApp/Booking/BookingService.java
+++ src/main/java/com/example/HotelBookingApp/Booking/BookingService.java
@@ -2,6 +2,18 @@ package com.example.HotelBookingApp.Booking;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookingService {
+
+    private BookingRepository bookingRepository;
+
+    public BookingService(BookingRepository bookingRepository){
+        this.bookingRepository = bookingRepository;
+    }
+
+    public List<BookingEntity> getAllBookings(){
+        return this.bookingRepository.findAll();
+    }
 }
